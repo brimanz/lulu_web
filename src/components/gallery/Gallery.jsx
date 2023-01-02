5b4f018ea1de6aaf3c6859edcbac2051a2e5d827
jonathan briceño <brimanz2014@gmail.com>
2023-01-02 12:33:20 -0400

    adding some filter styles

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -3,9 +3,20 @@ import styled from '@emotion/styled'
 
 
 const Gallery = () =>{
+
+	const genres = ["Todo", "Dama", "Caballero", "Niños", "Bebes"];
+
 	return(
 		<GalleryContainer>
 			<Title>Observa nuestro trabajo:</Title>
+
+
+			<ButtonsContianer>
+				{genres.map(item => (
+					<Button>{item}</Button>
+				))}	
+			</ButtonsContianer>
+			
 		</GalleryContainer>
 	);
 }
@@ -27,4 +38,32 @@ const Title = styled.h2`
 	font-size: 2.3rem;
 	font-weight: normal;
 	text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.7);
+`;
+const ButtonsContianer = styled.div`
+	margin-top: 2rem;
+	display: flex;
+	flex-wrap: wrap;
+	gap: 1;
+	justify-content: center;
+`;
+const Button = styled.button`
+	padding: 0.5rem;
+	margin: 1rem 1rem;
+	width: 5rem;
+
+	background-color: #283c86;
+	color: #f5f5f5;
+	font-weight: bold;
+	border: none;
+	border-radius: 0.4rem;
+	cursor: pointer;
+
+	&.active{
+		background-color: #b91d73;
+	}
+
+	:hover{
+		background-color: #b91d73;
+		box-shadow: 0px 3px 2px rgba(0, 0, 0, 0.3);
+	}
 `;
