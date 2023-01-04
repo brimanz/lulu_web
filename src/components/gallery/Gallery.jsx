9a3531be54b7e266443c3a17151cc3e17ea1b773
jonathan briceño <brimanz2014@gmail.com>
2023-01-04 07:30:00 -0400

    adding some animations

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -47,6 +47,7 @@ const Title = styled.h2`
 	color: #FF0099;
 	margin-top: 2rem;
 	font-size: 2.3rem;
+
 	font-weight: normal;
 	text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.7);
 `;
@@ -58,57 +59,91 @@ const ClothesContainer = styled.div`
 	margin-top: 4rem;
 	height: 30rem;
 	width: 100%;
+
+	@media (max-width: 800px){
+		display: flex;
+		flex-direction: column;
+		margin-top: 20rem;
+	}
 `
 const ClothesItem1 = styled.div`
 	height: 20rem;
 	background-image: url(../../.././public/assets/woman.png);
 	background-size: cover;
+	background-position: center;
 	border: 2px solid #FF0099;
-	width: 20%;
+	width: 5%;
 	opacity: 0.9;
 
 	:hover{
 		box-shadow: 6px 4px rgba(0, 0, 0, 0.25);
 		opacity: 1;
+		width: 20%;
+		transition: 0.5s ease;
+	}
+
+	@media (max-width: 800px){
+		width: 100%;
 	}
 `;
 const ClothesItem2 = styled.div`
 	height: 20rem;
 	background-image: url(../../.././public/assets/man.png);
 	background-size: cover;
+	background-position: center;
 	border: 2px solid #283c86;
-	width: 20%;
+	width: 5%;
 	opacity: 0.9;
 
 	:hover{
 		box-shadow: 6px 4px rgba(0, 0, 0, 0.25);
 		opacity: 1;
+		width: 20%;
+		transition: 0.5s ease;
+	}
+
+	@media (max-width: 800px){
+		width: 100%;
 	}
 `;
 const ClothesItem3 = styled.div`
 	height: 20rem;
 	background-image: url(../../.././public/assets/kid.png);
 	background-size: cover;
+	background-position: center;
 	border: 2px solid #f37335;
-	width: 20%;
+	width: 5%;
 	opacity: 0.9;
 
 	:hover{
 		box-shadow: 6px 4px rgba(0, 0, 0, 0.25);
 		opacity: 1;
+		width: 20%;
+		transition: 0.5s ease;
+	}
+
+	@media (max-width: 800px){
+		width: 100%;
 	}
 `;
 const ClothesItem4 = styled.div`
 	height: 20rem;
 	background-image: url(../../.././public/assets/baby.png);
 	background-size: cover;
+	background-position: center;
 	border: 2px solid #FF0000;
-	width: 20%;
+	width: 5%;
 	opacity: 0.9;
 
 	:hover{
 		box-shadow: 6px 4px rgba(0, 0, 0, 0.25);
 		opacity: 1;
+		width: 20%;
+		transition: 0.5s ease;
+	}
+
+	@media (max-width: 800px){
+		width: 100%;
 	}
 `;
 const ClothesTitle = styled.h2`
@@ -116,6 +151,7 @@ const ClothesTitle = styled.h2`
 	color: #ffffff;
 	margin-top: 8rem;
 	
+	writing-mode: vertical-lr;
 	font-size: 3rem;
 	text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.3);
 `;
